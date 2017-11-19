687c3fb9472385c75d82e4f8bb44c51aa80405c1
Raymond427 <rferrel427@gmail.com>
2017-11-19 11:24:34 -0500

    minimized imports in testLibraries

--- src/scripts/tests/testLibraries.js
+++ src/scripts/tests/testLibraries.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme from 'enzyme';
-import { shallow, mount, render } from 'enzyme';
+import Enzyme, { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 

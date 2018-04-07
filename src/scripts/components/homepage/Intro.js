2563db5ba05d6c2dd962d246fd679b6ca0e667bc
Raymond427 <rferrel427@gmail.com>
2018-04-07 01:48:48 -0400

    Spelling errors

--- src/scripts/components/homepage/Intro.js
+++ src/scripts/components/homepage/Intro.js
@@ -32,7 +32,7 @@ class Intro extends Component {
           <h1>Hi, I'm Raymond</h1>
           <h3>I'm a Community Volunteer, Entrepreneur, Software Engineer and
           Web Designer. I'm interested in creating software that helps
-          underpriviledged people find their passion and reach their potential.</h3>
+          underprivileged people find their passion and reach their potential.</h3>
           <Link className="Home-content__link" to='/resume'><button
           onClick={() => ReactGA.event({
             category: 'Homepage',
